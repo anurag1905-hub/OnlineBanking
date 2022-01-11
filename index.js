c2935c98160ee69e2dddce8feaaf2296f6c56457
anurag1905-hub <anuragharsh626@gmail.com>
2022-01-11 20:23:33 +0530

    Set up the View Engine

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const port = 8000;
+const port = 9000;
 const app = express();
 
+app.set('view engine','ejs');
+app.set('views','./views');
 
+app.use('/',require('./routes/index'));
 
 app.listen(port,function(err){
     if(err){
