c7cc16eba98a6d3d8a607b138f85e78316b13aea
anurag1905-hub <anuragharsh626@gmail.com>
2022-01-12 02:48:54 +0530

    Add the landing page

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 app.set('view engine','ejs');
 app.set('views','./views');
 
+app.use(express.static('assets'));  // to access static files
+
 app.use('/',require('./routes/index'));
 
 app.listen(port,function(err){
