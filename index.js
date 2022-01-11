654927012138e63b8d93dda848e081f8a3bde3d9
anurag1905-hub <anuragharsh626@gmail.com>
2022-01-11 20:16:54 +0530

    Set up directory structure

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const port = 8000;
+const app = express();
+
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log('Error in starting the server ',err);
+        return;
+    }
+    else{
+        console.log('Server is running successfully at port no : ',port);
+    }
+})
