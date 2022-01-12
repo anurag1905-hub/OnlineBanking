49c64587f9c63aba7342171437683b81c306af0d
anurag1905-hub <anuragharsh626@gmail.com>
2022-01-13 02:13:10 +0530

    set up mongoose

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const port = 9000;
+const cookieParser = require('cookie-parser'); // Allows us to read and write to a cookie
 const app = express();
+const db = require('./config/mongoose');
 
-app.set('view engine','ejs');
-app.set('views','./views');
+app.set('view engine','ejs');  //set up the view engine
+app.set('views','./views');    // specify a folder to look for the views.
 
 app.use(express.static('assets'));  // to access static files
+app.use(express.urlencoded());    // so that we can collect form data and store it in req.body 
+app.use(cookieParser());    // set up the cookie parser
 
 app.use('/',require('./routes/index'));
 
