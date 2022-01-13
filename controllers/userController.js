e583c0bbaf4b4efac39d933d3b4297c31952b788
anurag1905-hub <anuragharsh626@gmail.com>
2022-01-13 17:34:55 +0530

    Add code for user sign up

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 module.exports.login = function(req,res){
     return res.render('userLogin');
 }
@@ -8,4 +10,28 @@ module.exports.signup = function(req,res){
 
 module.exports.home = function(req,res){
     return res.render('home');
+}
+
+module.exports.create = function(req,res){
+   console.log(req.body.isAdmin);
+   User.findOne({email:req.body.email},function(err,user){
+      if(err){
+          console.log('Error in finding the user ',err);
+          return res.redirect('back');
+      }
+      if(user||req.body.isAdmin){
+          return res.redirect('back');
+      }
+      else{
+          User.create(req.body,function(err,user){
+             if(err){
+                 console.log('Error in creating the user ',err);
+                 return res.redirect('back');
+             }
+             else{
+                 return res.redirect('/user/login');
+             }
+          });
+      }
+   });
 }
--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,6 @@ const userController = require('../controllers/userController');
 router.get('/login',userController.login);
 router.get('/signup',userController.signup);
 router.get('/',userController.home);
+router.post('/create',userController.create);
 
 module.exports = router;
