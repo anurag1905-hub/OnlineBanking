38b1fbb30c7f9a2381dd2b271dcb2f8cceb759a6
anurag1905-hub <anuragharsh626@gmail.com>
2022-02-06 18:16:33 +0530

    make assetpath available in locals

---
+++ config/view-helper.js
@@ -0,0 +1,17 @@
+const env = require('./environment');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (app)=>{
+    app.locals.assetPath = function(filePath){
+        //console.log(filePath);
+        if(env.name=='development'){
+            return filePath;
+        }
+        else{
+            //console.log(path.join(__dirname,'../public/assets/rev-manifest.json'));
+            //console.log(JSON.parse(fs.readFileSync(path.join(__dirname,'../public/assets/rev-manifest.json'))))[filePath];
+            return '/'+JSON.parse(fs.readFileSync(path.join(__dirname,'../public/assets/rev-manifest.json')))[filePath];
+        }
+    }
+}
